cc061908f878d6108104b10e0450d21ac5ed1d2d
Ali Zand <alizand9224@gmail.com>
2020-07-25 21:05:18 -0700

    CD-1 Create the Home index and the css. delete unused files from the … (#2)
    * CD-1 Create the Home index and the css. delete unused files from the template, add fonts to the index.html
    
    * CD-1 Fix missing line at the end of the file
    
    * CD-1 Add compatibility for mobile phones
    
    Co-authored-by: Ali Zand <ali.zand9224@gmail.com>

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './index.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="title-container">
+          <div className="title-content title-creative">Creative</div>
+          <div className="title-content title-design">Design</div>
+        </div>
+        <div className="slogan-container">
+          YOU HAVE A DREAM<br />
+          WE MAKE IT COME TO LIFE
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
