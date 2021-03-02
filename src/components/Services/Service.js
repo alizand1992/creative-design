a123255d097c664f9e5bfd9a7a3473cb39c19da2
Ali Zand <ali.zand9224@gmail.com>
2021-03-01 20:47:33 -0800

    CD-6 Make it more nicer looking ;)

--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -8,8 +8,18 @@ const Service = ({ name, image }) => {
       <div className="service-image-container">
         <img src={image} className="service-image" alt={`${name} service`}/>
       </div>
-      <br />
-      {name}
+      <div>
+        <div className="service-title">{name}</div>
+        <div className="service-content">
+          LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. OMNES ENIM IUCUNDUM MOTUM,
+          QUO SENSUS HILARETUR. QUAE CUM DIXISSET PAULUMQUE INSTITISSET, QUID EST? ECCE
+          ALIUD SIMILE DISSIMILE. MIHI QUIDEM ANTIOCHUM, QUEM AUDIS, SATIS BELLE VIDERIS ATTENDERE.
+          ATQUE HAEC ITA IUSTITIAE PROPRIA SUNT, UT SINT VIRTUTUM RELIQUARUM COMMUNIA. SIN KAKAN
+          MALITIAM DIXISSES, AD ALIUD NOS UNUM CERTUM VITIUM CONSUETUDO LATINA TRADUCERET.
+          DUO REGES: CONSTRUCTIO INTERRETE. AUT, PYLADES CUM SIS, DICES TE ESSE ORESTEM, UT
+          MORIARE PRO AMICO?
+        </div>
+      </div>
     </div>
   );
 };
